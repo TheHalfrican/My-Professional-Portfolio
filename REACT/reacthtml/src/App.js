e95c107745c84163481bc8896867b5ebd0a78d89
Noah Mattocks <noahmattocks@gmail.com>
2022-08-30 16:29:36 -0700

    Uploading finished work from my other github accounts repo

---
+++ REACT/reacthtml/src/App.js
@@ -0,0 +1,16 @@
+import Home from './pages/Home'
+import './App.css';
+import { Button } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <body>
+      <Home />
+      <Button>Test Button</Button>
+    </body>
+   
+  );
+}
+
+export default App;
