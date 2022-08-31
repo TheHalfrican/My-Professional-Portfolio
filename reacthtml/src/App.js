d4fc41064c1a542c8d2ceff142ce457568aa5a52
Noah Mattocks <noahmattocks@gmail.com>
2022-08-30 18:57:53 -0700

    Moved folder to correct position

---
+++ reacthtml/src/App.js
@@ -0,0 +1,14 @@
+import Home from './pages/Home'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <body>
+      <Home />
+    </body>
+   
+  );
+}
+
+export default App;
